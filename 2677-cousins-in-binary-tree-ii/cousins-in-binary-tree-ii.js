638d70844db1a723f812c7c6bb87fe753a7d3b93
SanoferNiswan <132045268+SanoferNiswan@users.noreply.github.com>
2024-10-24 10:23:33 +0530

    Time: 302 ms (100.00%) | Memory: 130.6 MB (7.14%) - LeetSync

--- 2677-cousins-in-binary-tree-ii/cousins-in-binary-tree-ii.js
+++ 2677-cousins-in-binary-tree-ii/cousins-in-binary-tree-ii.js
@@ -10,42 +10,43 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
+var replaceValueInTree = function(root) {
+    root.val=0;
+    dfs([root]);
 
-var replaceValueInTree = function (root) {
-    function dfs(arr) {
-        if (arr.length == 0) return
+    return root;
 
-        let sum = 0
+    function dfs(arr){
+        if(arr.length==0) return;
 
-        for (let node of arr) {
-            if (!node) continue
-            if (node.left) sum += node.left.val
-            if (node.right) sum += node.right.val
+        sum=0;
+        for(let node of arr){
+            if(!node) continue;
+            if(node.left) sum+=node.left.val;
+            if(node.right) sum+=node.right.val; 
         }
 
-        let childArr = []
+        cursum=0;
+        childarr=[];
 
-        for (let node of arr) {
-            let curSum = 0
+        for(let node of arr){
 
-            if (node.left) curSum += node.left.val
-            if (node.right) curSum += node.right.val
+            if(node.left) cursum+=node.left.val;
+            if(node.right) cursum+=node.right.val;
 
-            if (node.left) {
-                node.left.val = sum - curSum
-                childArr.push(node.left)
+            if(node.left){
+                node.left.val=sum-cursum;
+                childarr.push(node.left);
             }
-            if (node.right) {
-                node.right.val = sum - curSum
-                childArr.push(node.right)
+            if(node.right){
+                node.right.val=sum-cursum;
+                childarr.push(node.right);
             }
+
+            cursum=0;
+
         }
 
-        dfs(childArr)
+        dfs(childarr);
     }
-
-    root.val = 0
-    dfs([root])
-
-    return root
 };
