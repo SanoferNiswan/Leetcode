3744005b4c793e7c926fa3ab70241facfc2bf192
SanoferNiswan <132045268+SanoferNiswan@users.noreply.github.com>
2024-11-21 11:18:12 +0530

    Time: 0 ms (100.00%) | Memory: 48.9 MB (29.33%) - LeetSync

---
+++ 1041-available-captures-for-rook/available-captures-for-rook.js
@@ -0,0 +1,46 @@
+/**
+ * @param {character[][]} board
+ * @return {number}
+ */
+var numRookCaptures = function(board) {
+    var i,j,f=0;
+    for(i=0;i<board.length;i++){
+        for(j=0;j<board[0].length;j++){
+            if(board[i][j]=='R'){
+                f=1;
+                break;
+            }
+        }
+        if(f==1) break;
+    }
+    var count=0;
+    for(let x=j-1;x>=0;x--){
+        if(board[i][x]=='p'){
+            count++;
+            break;
+        }
+        else if(board[i][x]=='B') break;
+    }
+    for(let x=j+1;x<8;x++){
+        if(board[i][x]=='p'){
+            count++;
+            break;
+        }
+        else if(board[i][x]=='B') break;
+    }
+    for(let x=i+1;x<8;x++){
+        if(board[x][j]=='p'){
+            count++;
+            break;
+        }
+        else if(board[x][j]=='B') break;
+    }
+    for(let x=i-1;x>=0;x--){
+        if(board[x][j]=='p'){
+            count++;
+            break;
+        }
+        else if(board[x][j]=='B') break;
+    }
+    return count;
+};
