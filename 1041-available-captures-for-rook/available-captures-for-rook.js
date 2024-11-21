f7696838a96f2049a5f78adabbfff02deca42f17
SanoferNiswan <132045268+SanoferNiswan@users.noreply.github.com>
2024-11-21 11:20:30 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (81.33%) - LeetSync

--- 1041-available-captures-for-rook/available-captures-for-rook.js
+++ 1041-available-captures-for-rook/available-captures-for-rook.js
@@ -4,6 +4,7 @@
  */
 var numRookCaptures = function(board) {
     var i,j,f=0;
+    // check for rook
     for(i=0;i<board.length;i++){
         for(j=0;j<board[0].length;j++){
             if(board[i][j]=='R'){
@@ -14,6 +15,8 @@ var numRookCaptures = function(board) {
         if(f==1) break;
     }
     var count=0;
+
+    // check left
     for(let x=j-1;x>=0;x--){
         if(board[i][x]=='p'){
             count++;
@@ -21,6 +24,8 @@ var numRookCaptures = function(board) {
         }
         else if(board[i][x]=='B') break;
     }
+
+    // check right
     for(let x=j+1;x<8;x++){
         if(board[i][x]=='p'){
             count++;
@@ -28,6 +33,8 @@ var numRookCaptures = function(board) {
         }
         else if(board[i][x]=='B') break;
     }
+
+    // check bottom
     for(let x=i+1;x<8;x++){
         if(board[x][j]=='p'){
             count++;
@@ -35,6 +42,8 @@ var numRookCaptures = function(board) {
         }
         else if(board[x][j]=='B') break;
     }
+
+    // check top
     for(let x=i-1;x>=0;x--){
         if(board[x][j]=='p'){
             count++;
