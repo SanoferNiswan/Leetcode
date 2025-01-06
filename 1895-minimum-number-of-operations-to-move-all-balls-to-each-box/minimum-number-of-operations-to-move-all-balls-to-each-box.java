d8a9f70ea98a9f57b77ed9280d3ad6d50887f58a
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2025-01-06 11:11:19 +0530

    Time: 4 ms (76.56%) | Memory: 44.9 MB (46.88%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,26 +1,24 @@
 class Solution {
     public int[] minOperations(String boxes) {
-        int n = boxes.length();
-        int[] ans = new int[n];
-        int count = 0, ops = 0;
-        
-        // Left to right pass
-        for (int i = 0; i < n; i++) {
-            ans[i] = ops;
-            count += boxes.charAt(i) == '1' ? 1 : 0;
-            ops += count;
+
+        int ans[] = new int[boxes.length()];
+        int ops = 0 , count = 0;
+
+        for(int i = 0 ; i < boxes.length() ; i++){
+            ans [i] = ops;
+            count+=boxes.charAt(i)=='1'?1:0;
+            ops+=count;
         }
-        
-        count = 0;
+
         ops = 0;
-        
-        // Right to left pass
-        for (int i = n - 1; i >= 0; i--) {
-            ans[i] += ops;
-            count += boxes.charAt(i) == '1' ? 1 : 0;
-            ops += count;
+        count = 0;
+
+        for(int i = boxes.length()-1 ; i >= 0 ; i--){
+            ans[i]+=ops;
+            count+=boxes.charAt(i)=='1'?1:0;
+            ops+=count;
         }
-        
+
         return ans;
     }
 }
