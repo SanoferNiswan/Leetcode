0ce78e07698fdb4a00186fc721dc8bd01073ecce
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2025-01-06 12:10:17 +0530

    Time: 485 ms (15.08%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,4 +1,1 @@
-select * from Cinema 
-where Mod(id,2) != 0 
-and description != 'boring'
-order by rating desc;
+select * from Cinema where Mod(id,2) != 0 and description != 'boring' order by rating desc;
