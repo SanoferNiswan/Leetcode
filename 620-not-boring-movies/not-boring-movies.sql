b941e0890c425b0803a56946f1bf905f90f6bb50
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2025-01-06 12:09:28 +0530

    Time: 508 ms (12.99%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
--- select * from Cinema order by id where id%2!=0 && description!="boring";
 select * from Cinema 
 where Mod(id,2) != 0 
 and description != 'boring'
