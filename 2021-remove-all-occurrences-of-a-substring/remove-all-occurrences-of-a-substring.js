a1c0ec0dd61c8e987d72f2486bff20c931c96403
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2025-02-11 09:13:21 +0530

    Time: 17 ms (14.29%) | Memory: 55.7 MB (7.52%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.js
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.js
@@ -3,6 +3,33 @@
  * @param {string} part
  * @return {string}
  */
-const removeOccurrences = f = (s,part) => {
-   return s.includes(part) ? f(s.replace(part,''),part):s;
+
+var removeOccurrences = function(s,part){
+    while(true){
+        let index = -1;
+        for(let i = 0 ; i <= s.length-part.length;i++){
+            let match = true;
+            for(let j = 0;j<part.length;j++){
+                if(s[i+j]!==part[j]){
+                    match=false;
+                    break;
+                }
+            }
+            if(match){
+                index=i;
+                break;
+            }
+        }
+
+        if(index===-1) break;
+
+        let newstr="";
+        for(let i=0;i<s.length;i++){
+            if(i>=index && i<index+part.length) continue;
+            newstr+=s[i];
+        }
+
+        s=newstr;
+    }
+    return s;
 }
