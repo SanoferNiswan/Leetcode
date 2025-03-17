dbe618ca2983b7dc2d7b1977f310d5817fbd962f
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2025-03-17 11:23:11 +0530

    Time: 32 ms (5.07%) | Memory: 59.5 MB (5.53%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.js
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.js
@@ -6,7 +6,6 @@ var divideArray = function(nums) {
     nums.sort();
     console.log(nums);
     for(let i=0;i<nums.length-1;i=i+2){
-        console.log(i,":",nums[i],":",i+1,":",nums[i+1])
         if((nums[i+1]!==nums[i])){
             return false;
         };
