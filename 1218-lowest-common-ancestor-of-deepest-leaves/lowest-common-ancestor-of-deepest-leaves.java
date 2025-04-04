cf604cc795686a7191458d51f53f05c0753f5810
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2025-04-04 10:51:19 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (39.73%) - LeetSync

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode res;
+    int maxDepth = -1;
+
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        dfs(root,0);
+        return res;
+    }
+
+    public int dfs(TreeNode node,int depth){
+        if(node==null){
+            maxDepth = Math.max(depth,maxDepth);
+            return depth;
+        }
+
+        int left = dfs(node.left,depth+1);
+        int right = dfs(node.right,depth+1);
+
+        if(left==right && left == maxDepth){
+            res = node;
+        }
+
+        return Math.max(left,right);
+    }
+}
