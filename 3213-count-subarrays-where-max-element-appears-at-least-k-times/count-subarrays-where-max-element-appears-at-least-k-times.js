7dc83668ac4b671864582ddec3a2fd232454a521
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2025-04-29 16:02:29 +0530

    Time: 13 ms (42.62%) | Memory: 68.2 MB (80.33%) - LeetSync

---
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var countSubarrays = function(nums, k) {
+    const maxElement = Math.max(...nums);
+    let res = 0, left = 0, maxiCount = 0;
+    
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] === maxElement) maxiCount++;
+        while (maxiCount === k) {
+            res += nums.length - right;
+            if (nums[left] === maxElement) maxiCount--;
+            left++;
+        }
+    }
+    
+    return res;
+};
