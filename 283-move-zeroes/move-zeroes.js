94a6cf59b143babfb8905523783f6f9e2fe1d514
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2025-09-08 15:41:09 +0530

    Time: 1 ms (77.99%) | Memory: 59.3 MB (81.65%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -1,15 +1,12 @@
-/**
- * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
- */
 var moveZeroes = function(nums) {
-    let left = 0 ;
-    for(let right = 0 ; right < nums.length ; right++){
+    let left = 0 , right = 0;
+    while(right<nums.length){
         if(nums[right]!=0){
             let temp = nums[right];
             nums[right] = nums[left];
             nums[left] = temp;
             left++;
         }
+        right++;
     }
 };
