3aef87e24e75564120e02110f92316b4b47ad0cf
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2025-09-08 18:22:40 +0530

    Time: 4 ms (77.36%) | Memory: 57 MB (76.63%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -3,21 +3,19 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    s=s.toLowerCase();
-    let str=""
-    for(let i=0;i<s.length;i++){
-        let char = s[i].charCodeAt(0);
-        if((char>=97 && char<=122) || (char>=48 && char<=57)){
-            str+=s[i];
-        }
-    }
-    let left = 0,right = str.length-1;
+    let left=0;right=s.length-1;
+    s = s.toLowerCase();
     while(left<right){
-        if(str[left]!=str[right]){
-            return false;
-        }
+        while(left<right && !isAlphaNumeric(s[left])) left++;
+        while(left<right && !isAlphaNumeric(s[right])) right--;
+        if(s[left]!==s[right]) return false;
         left++;
         right--;
     }
     return true;
 };
+
+const isAlphaNumeric = (char) => {
+    let code = char.charCodeAt(0);
+    return ((code>=97 && code <= 122) || (code>=48 && code<=57))
+}
