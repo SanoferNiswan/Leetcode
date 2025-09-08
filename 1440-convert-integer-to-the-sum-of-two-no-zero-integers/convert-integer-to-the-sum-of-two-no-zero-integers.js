7a438a7e1abed152f61feac7f427d2b2b9247888
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2025-09-08 12:03:21 +0530

    Time: 0 ms (100.00%) | Memory: 55 MB (17.39%) - LeetSync

---
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var getNoZeroIntegers = function(n) {
+    let a = [];
+    for(let i=1;i<=n/2;i++){
+        if(!i.toString().includes('0') && !(n-i).toString().includes('0')){
+            return [i,n-i]
+        }
+    }
+    return a;
+};
