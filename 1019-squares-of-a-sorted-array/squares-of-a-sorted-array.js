a0f10b7bbd0183b85a7a53ca3612006fcbd86dd1
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2025-09-09 12:48:59 +0530

    Time: 3 ms (81.52%) | Memory: 61.2 MB (82.25%) - LeetSync

---
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -0,0 +1,14 @@
+var sortedSquares = function(nums) {
+    let res = new Array(nums.length).fill(0), left = 0 , right = nums.length-1;
+    for(let i=nums.length-1;i>=0;i--){
+        if(Math.abs(nums[left])>Math.abs(nums[right])){
+            res[i]=nums[left]*nums[left];
+            left++;
+        }
+        else{
+            res[i]=nums[right]*nums[right];
+            right--;
+        }
+    }
+    return res;
+};
