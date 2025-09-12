10af1e0f84a5d6520bd21c04da078aa1ced82fed
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2025-09-12 16:59:37 +0530

    Time: 0 ms (100.00%) | Memory: 55.8 MB (20.54%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -1,18 +1,11 @@
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number[]}
- */
 var intersection = function(nums1, nums2) {
-    let s = new Set();
-    let res = [];
-    for(let i=0;i<nums1.length;i++){
-        for(let j=0;j<nums2.length;j++){
-            if(nums1[i]==nums2[j] && !s.has(nums1[i])){
-                s.add(nums1[i]);
-                res.push(nums1[i]);
-            }
-        }
+    let s1 = new Set(nums1);
+    let s2 = new Set(nums2);
+    let result = [];
+
+    for(let i of s1){
+        if(s2.has(i)) result.push(i);
     }
-    return res;
+
+    return result;
 };
