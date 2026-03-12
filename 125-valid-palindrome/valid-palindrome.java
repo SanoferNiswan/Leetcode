aeb03431d8e13a78dcb5db99175b5c06eefdfe70
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2026-03-12 07:55:06 +0530

    Time: 4 ms (49.58%) | Memory: 45.1 MB (42.14%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String alphabets="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",processedString="";
-        for(int i=0;i<s.length();i++){
-            if(alphabets.contains(String.valueOf(s.charAt(i)))) processedString+=s.charAt(i);
+        StringBuilder processed = new StringBuilder();
+        for(char c:s.toCharArray()){
+            if(Character.isLetterOrDigit(c)){
+                processed.append(Character.toLowerCase(c));
+            }
         }
-
-        int left = 0, right = processedString.length()-1;
-        processedString = processedString.toLowerCase();
+        int left = 0, right = processed.length()-1;
         while(left<right){
-            if(processedString.charAt(left) != processedString.charAt(right)) return false;
+            if(processed.charAt(left) != processed.charAt(right)) return false;
             left++; right--;
         }
 
