256bacffc93bca25f0fb3f3f73cac254fbc44be0
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2026-03-19 14:11:47 +0530

    Time: 51 ms (17.30%) | Memory: 47.9 MB (5.21%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s2.length()<s1.length()) return false;
+        Map<Character, Integer> s1Count = new HashMap<Character, Integer>();
+        Map<Character, Integer> s2Count = new HashMap<Character, Integer>();
+        int k = s1.length();
+        boolean found = true;
+        for(int i=0;i<k;i++){
+            s1Count.put(s1.charAt(i), s1Count.getOrDefault(s1.charAt(i),0)+1);
+            s2Count.put(s2.charAt(i), s2Count.getOrDefault(s2.charAt(i),0)+1);
+        }
+
+        for(Map.Entry<Character, Integer> entry : s1Count.entrySet()){
+            char curr = entry.getKey();
+            if(!s1Count.get(curr).equals(s2Count.getOrDefault(curr,0))){
+                found = false;
+                break;
+            }
+        }
+        if(found) return true;
+
+        for(int i=k;i<s2.length();i++){
+            char left = s2.charAt(i-k), right = s2.charAt(i);
+            s2Count.put(left, s2Count.get(left)-1);
+            if(s2Count.get(left)==0) s2Count.remove(left);
+            s2Count.put(right, s2Count.getOrDefault(right,0)+1);
+            found = true;
+
+            for(Map.Entry<Character, Integer> entry : s1Count.entrySet()){
+                char curr = entry.getKey();
+                if(!s1Count.get(curr).equals(s2Count.getOrDefault(curr,0))){
+                    found = false;
+                    break;
+                }
+            }
+            if(found) return true;
+        }
+        return false;
+    }
+}
