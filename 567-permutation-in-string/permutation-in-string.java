61f29e32064f92db52783cfb5926511e658dea8e
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2026-03-20 07:51:10 +0530

    Time: 6 ms (87.82%) | Memory: 44.1 MB (60.09%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,30 +1,29 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s2.length()<s1.length()) return false;
-        Map<Character, Integer> s1Count = new HashMap<Character, Integer>();
-        Map<Character, Integer> s2Count = new HashMap<Character, Integer>();
-        int k = s1.length();
-        for(int i=0;i<k;i++){
-            s1Count.put(s1.charAt(i), s1Count.getOrDefault(s1.charAt(i),0)+1);
-            s2Count.put(s2.charAt(i), s2Count.getOrDefault(s2.charAt(i),0)+1);
-        }
+        if(s2.length() < s1.length()) return false;
 
-        for(Map.Entry<Character, Integer> entry : s1Count.entrySet()){
-            char curr = entry.getKey();
-            if(s1Count.equals(s2Count)) return true;
-        }
+        int freq[] = new int[26];
+        for(char c : s1.toCharArray()) freq[c-'a']++;
 
-        for(int i=k;i<s2.length();i++){
-            char left = s2.charAt(i-k), right = s2.charAt(i);
-            s2Count.put(left, s2Count.get(left)-1);
-            if(s2Count.get(left)==0) s2Count.remove(left);
-            s2Count.put(right, s2Count.getOrDefault(right,0)+1);
+        int left = 0, right = 0, count = s1.length();
 
-            for(Map.Entry<Character, Integer> entry : s1Count.entrySet()){
-                char curr = entry.getKey();
-                if(s1Count.equals(s2Count)) return true;
+        while(right < s2.length()){
+            char curr = s2.charAt(right);
+
+            if(freq[curr-'a'] > 0) count--;
+
+            freq[curr-'a']--;
+            right++;
+
+            if(count==0) return true;
+
+            if(right-left == s1.length()){
+                curr = s2.charAt(left);
+                if(freq[curr-'a']>=0) count++;
+                freq[curr-'a']++;
+                left++;
             }
-        }
+        } 
         return false;
     }
 }
