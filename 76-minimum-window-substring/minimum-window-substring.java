2ffef5c406cac93080c7363ada09b0684b4928e4
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2026-03-24 21:30:13 +0530

    Time: 18 ms (44.65%) | Memory: 47.1 MB (54.50%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,33 +1,33 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(t.length() > s.length() ) return "";
+        if(t.length() > s.length()) return "";
         Map<Character, Integer> m = new HashMap<Character, Integer>();
 
-        for(char c:t.toCharArray()) m.put(c,m.getOrDefault(c,0)+1);
+        for(char c:t.toCharArray()) m.put(c, m.getOrDefault(c,0)+1);
 
-        int left = 0, right = 0, minLength = s.length()+1, start = -1, count = t.length();
+        int left =0, right =0, count = t.length();
+        int start = -1, minLength = s.length()+1;
 
         while(right<s.length()){
             char curr = s.charAt(right);
+
             if(m.containsKey(curr)){
                 if(m.get(curr)>0) count--;
-
-                m.put(curr,m.get(curr)-1);
+                m.put(curr, m.get(curr)-1);
             }
 
             right++;
 
-            while(count==0){
-
-                if(right-left < minLength){
-                    minLength = right - left;
-                    start = left;
-                }
+            while(count == 0){
                 curr = s.charAt(left);
 
-                if(m.containsKey(curr)){
-                    m.put(curr,m.get(curr)+1);
+                if(right-left < minLength){
+                    minLength = right-left;
+                    start = left;
+                }
 
+                if(m.containsKey(curr)){
+                    m.put(curr, m.get(curr)+1);
                     if(m.get(curr)>0) count++;
                 }
 
@@ -35,6 +35,6 @@ class Solution {
             }
         }
 
-        return start == -1 ? "" : s.substring(start, start+minLength);
+        return start==-1?"":s.substring(start,start+minLength);
     }
 }
