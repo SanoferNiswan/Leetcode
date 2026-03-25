e054e3a6f40125087bcb00df4ce90e866bf39656
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2026-03-25 13:56:22 +0530

    Time: 38 ms (90.54%) | Memory: 69.4 MB (78.79%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,23 +1,25 @@
 class Solution {
     public int totalFruit(int[] fruits) {
         Map<Integer, Integer> m = new HashMap<Integer, Integer>();
-        int left = 0, right = 0, max = 0;
-        for(right = 0;right < fruits.length; right++){
-            m.put(fruits[right], right);
+        int left = 0, max = 0;
+        for(int right = 0 ; right < fruits.length ; right++){
+            m.put(fruits[right],right);
 
             if(m.size() > 2){
-                int minIndex = fruits.length+1, removeElement = 0;
-                for(Map.Entry<Integer, Integer> entry: m.entrySet()){
-                    if(entry.getValue()<minIndex){
-                        minIndex = entry.getValue();
-                        removeElement = entry.getKey();
+                int minLength = fruits.length + 1, toRemove = 0;
+                for(Map.Entry<Integer, Integer> entry:m.entrySet()){
+                    if(entry.getValue() < minLength){
+                        minLength = entry.getValue();
+                        toRemove = entry.getKey();
                     }
                 }
-                left = minIndex+1;
-                m.remove(removeElement);
+                left = minLength+1;
+                m.remove(toRemove);
             }
+
             max = Math.max(max, right-left+1);
         }
+
         return max;
     }
 }
