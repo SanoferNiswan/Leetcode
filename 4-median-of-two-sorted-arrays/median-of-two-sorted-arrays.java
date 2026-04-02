531ff263b199109c666d3dc3bf206baf766f1ec5
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2026-04-02 14:17:42 +0530

    Time: 1 ms (99.97%) | Memory: 48.8 MB (62.41%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,16 +1,28 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int left = 0, right = 0, ind = 0;
-        int temp[] = new int[nums1.length+nums2.length];
-        while(left<nums1.length && right < nums2.length){
-            if(nums1[left]>=nums2[right]) temp[ind++] = nums2[right++];
-            else temp[ind++] = nums1[left++];
-        }
-        while(left<nums1.length) temp[ind++] = nums1[left++];
-        while(right<nums2.length) temp[ind++] = nums2[right++];
+        int n1 = nums1.length, n2 = nums2.length;
+        if(n1>n2) return findMedianSortedArrays(nums2, nums1);
+        int low = 0 , high = n1;
+        while(low <= high){
+            int mid1 = (low+high)/2;
+            int mid2 = (n1+n2+1)/2 - mid1;
 
-        int n = temp.length;
-        if(n%2!=0) return temp[n/2];
-        else return (double)(temp[n/2 - 1]+temp[n/2])/2;
+            int l1 = mid1==0 ? Integer.MIN_VALUE : nums1[mid1-1];
+            int l2 = mid2==0 ? Integer.MIN_VALUE : nums2[mid2-1];
+
+            int r1 = mid1==n1 ? Integer.MAX_VALUE : nums1[mid1];
+            int r2 = mid2==n2 ? Integer.MAX_VALUE : nums2[mid2];
+
+            if(l1<=r2 && l2<=r1){
+                if((n1+n2)%2==0){
+                    return (Math.max(l1, l2) + Math.min(r1, r2))/2.0;
+                }else return Math.max(l1, l2);
+            }
+
+            else if(l1>r2) high = mid1-1;
+            else low = mid1+1; 
+        }
+
+        return 0;
     }
 }
