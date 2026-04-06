632d0a80e40e2f66d0b4a1756efae4668f4b7871
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2026-04-06 11:16:41 +0530

    Time: 6 ms (14.29%) | Memory: 47.1 MB (56.43%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -22,12 +22,3 @@ class MinStack {
         return st.isEmpty() ? -1 : st.peek();
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
