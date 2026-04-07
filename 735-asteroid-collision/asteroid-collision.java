3dc7175dcb3bed56083a3d1b3cbc72a04331ad1f
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2026-04-07 16:56:25 +0530

    Time: 5 ms (65.99%) | Memory: 47.1 MB (33.62%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,20 +1,24 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> st = new Stack<Integer>();
-        for(int num:asteroids){
-            if(num>=0) st.push(num);
-            else{
-                while(!st.isEmpty() && st.peek()>=0 && (num*-1)>st.peek()){
+        for (int num : asteroids) {
+            if (num >= 0)
+                st.push(num);
+            else {
+                while (!st.isEmpty() && st.peek() > 0 && -num > st.peek()) {
                     st.pop();
                 }
-                if(!st.isEmpty() && st.peek()==num*-1) st.pop();
-                else if(st.isEmpty() || !(st.peek()>=0)) st.push(num);
+                if (!st.isEmpty() && st.peek() == -num)
+                    st.pop();
+                else if (st.isEmpty() || !(st.peek() > 0))
+                    st.push(num);
             }
         }
 
         int[] res = new int[st.size()];
         int i = 0;
-        for(int num:st) res[i++] = num;
+        for (int num : st)
+            res[i++] = num;
 
         return res;
     }
