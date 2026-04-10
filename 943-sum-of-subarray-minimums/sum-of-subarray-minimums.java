95ecf549fa60e9a2039bcb80311cfe549795c26f
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2026-04-10 22:39:09 +0530

    Time: 38 ms (80.07%) | Memory: 52 MB (87.03%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,32 +1,22 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        int mod = 1000000007, n = arr.length;
-        int left[] = new int[n];
-        int right[] = new int[n];
         Stack<Integer> st = new Stack<Integer>();
-
-        for(int i = 0 ; i < n ; i++){
-            while(!st.isEmpty() && arr[st.peek()] > arr[i]) st.pop();
-            left[i] = st.isEmpty() ? -1 : st.peek();
-            st.push(i);
-        }
-
-        st.clear();
-
-        for(int i=n-1; i >= 0 ; i--){
-            while(!st.isEmpty() && arr[st.peek()] >= arr[i]) st.pop();
-            right[i] = st.isEmpty() ? n : st.peek();
-            st.push(i);
-        }
-
+        int n = arr.length, mod = 1000000007;
         long res = 0;
 
-        for(int i = 0 ; i < n ; i++){
-            int leftLen = i - left[i];
-            int rightLen = right[i] - i;
+        for(int i = 0 ; i <= n ; i++){
+            int curr = i==n ? 0 : arr[i];
 
-            long contribution = (long) arr[i] * leftLen * rightLen;
-            res = ( res + contribution ) % mod;
+            while(!st.isEmpty() && arr[st.peek()] > curr){
+                int mid = st.pop();
+                int left = st.isEmpty() ? -1 : st.peek();
+                int right = i;
+
+                long contribute = (long)arr[mid] * (left - mid) * (mid - right);
+                res = (res + contribute) % mod;
+            }
+
+            st.push(i);
         }
 
         return (int)res;
