6c73eeff08b6f96e557fd7a84987586e375cde1b
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2026-04-14 22:04:43 +0530

    Time: 14 ms (71.21%) | Memory: 47.8 MB (22.80%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,26 +1,20 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> m = new HashMap<Integer,Integer>();
-        for(int num:nums){
-            m.put(num, m.getOrDefault(num,0)+1);
+        Map<Integer,Integer> m = new HashMap<Integer, Integer>();
+
+        for(int num:nums) m.put(num, m.getOrDefault(num, 0)+1);
+
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b)->a[1] - b[1]);
+        for(int key:m.keySet()){
+            minHeap.add(new int[]{key, m.get(key)});
+
+            if(minHeap.size() > k) minHeap.poll();
         }
-        int[] a = new int[m.size()];
-        int i = 0;
-        for(Map.Entry<Integer,Integer> entry : m.entrySet()){
-            a[i]=entry.getValue();
-            i++;
-        }
-        Arrays.sort(a);
+
         int[] res = new int[k];
-        for(i=0;i<k;i++){
-                int count = a[a.length-1-i];
-                for(Map.Entry<Integer,Integer> entry : m.entrySet()){
-                    if(entry.getValue()==count){
-                        res[i] = entry.getKey();
-                        m.remove(res[i]);
-                        break;
-                    }
-            }
+        int i = 0;
+        while(!minHeap.isEmpty()){
+            res[i++] = minHeap.poll()[0];
         }
 
         return res;
