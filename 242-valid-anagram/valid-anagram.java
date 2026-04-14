3341c10cd2e69739575b09a1db1fc540f7a5a871
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2026-04-14 23:46:15 +0530

    Time: 6 ms (43.67%) | Memory: 44.3 MB (76.95%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,24 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
-        Map<Character,Integer> m = new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            if(m.containsKey(s.charAt(i))){
-                m.put(s.charAt(i), m.get(s.charAt(i))+1);
-            }else{
-                m.put(s.charAt(i),1);
-            }
+        if(s.length() != t.length()) return false;
+        int[] count = new int[26];
+
+        for(int i = 0 ; i < s.length() ; i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
         }
-        System.out.println("hm:"+m);
-        for(int i=0;i<t.length();i++){
-            if(m.containsKey(t.charAt(i))){
-                int count = m.get(t.charAt(i));
-                if(count > 1) m.put(t.charAt(i),count-1);
-                else m.remove(t.charAt(i));
-            }else{
-                return false;
-            }
-        }
+
+        for(int c:count) if(c!=0) return false;
+
         return true;
     }
 }
