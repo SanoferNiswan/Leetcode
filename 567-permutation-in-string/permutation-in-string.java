6d388edad6daf6975be0f88e5259813fd711af9b
Sanofer Niswan S <132045268+SanoferNiswan@users.noreply.github.com>
2026-04-15 12:58:54 +0530

    Time: 6 ms (87.69%) | Memory: 44.2 MB (42.97%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,29 +1,28 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s2.length() < s1.length()) return false;
+        if(s1.length() > s2.length()) return false;
+        int[] freq = new int[26];
+        for(char c:s1.toCharArray()) freq[c-'a']++;
 
-        int freq[] = new int[26];
-        for(char c : s1.toCharArray()) freq[c-'a']++;
-
-        int left = 0, right = 0, count = s1.length();
-
-        while(right < s2.length()){
+        int k = s1.length();
+        int left = 0, right = 0, count = k;
+        while(right<s2.length()){
             char curr = s2.charAt(right);
 
-            if(freq[curr-'a'] > 0) count--;
-
+            if(freq[curr-'a']>0) count--;
             freq[curr-'a']--;
             right++;
 
             if(count==0) return true;
 
-            if(right-left == s1.length()){
-                curr = s2.charAt(left);
-                if(freq[curr-'a']>=0) count++;
-                freq[curr-'a']++;
+            if(right-left>=k){
+                char l = s2.charAt(left);
+                if(freq[l-'a']>=0) count++;
+                freq[l-'a']++;
                 left++;
             }
-        } 
+        }
+
         return false;
     }
 }
